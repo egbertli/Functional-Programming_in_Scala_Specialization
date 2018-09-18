3281ccc53e0eb8b79f7bba90c94218c06af46788
egbertli <egbert1121@gmail.com>
2018-09-18 22:04:15 +1000

    nice assignment

--- Functional_Programming_Principles_In_Scala/recfun/src/main/scala/recfun/Main.scala
+++ Functional_Programming_Principles_In_Scala/recfun/src/main/scala/recfun/Main.scala
@@ -8,20 +8,57 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println()
+    println("Balance")
+    println(":-)".toList)
+    println(balance(":-)".toList))
+    println(balance("(a)".toList))
+    println()
+
+    println("Count Change")
+    print(countChange(4, List(1,2)))
+    println()
+
   }
 
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if (c == 0 || c == r) 1
+      else pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
+
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      //@tailrec
+      def loop(acc: Int, chars: List[Char]): Int = {
+        if (chars.isEmpty) acc
+        else if (chars.head == '(')  loop(acc + 1, chars.tail)
+        else if (chars.head == ')' && acc > 0) loop(acc - 1, chars.tail)
+        else if (chars.head == ')' && acc <= 0) -1
+        else loop(acc, chars.tail)
+      }
+
+      if (loop(0, chars) == 0) true else false
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def countChangeHelper(money: Int, coins: List[Int]): Int = {
+        if (money == 0) 1
+        else if (coins.isEmpty) 0
+        else if (money < 0 && !coins.isEmpty) 0
+        else countChangeHelper(money - coins.head, coins) + countChangeHelper(money, coins.tail)
+      }
+
+      if (coins.isEmpty || money <= 0) 0
+      else countChangeHelper(money, coins)
+    }
   }
